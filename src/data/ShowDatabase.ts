b63de73792f4256b58a13de57af558ef04259d7e
Andrea Soares <asoaresdev@gmail.com>
2023-03-17 20:20:59 -0300

    testando

---
+++ src/data/ShowDatabase.ts
@@ -0,0 +1,51 @@
+import { CustomError } from "../error/CustomError";
+import { BandNotFound, NoShowScheduled } from "../error/ShowError";
+import { getShow } from "../model/show/getShow";
+import { Show } from "../model/show/show";
+import { BaseDatabase } from "./BaseDatabase";
+
+export class ShowDatabse extends BaseDatabase{
+    createShow = async(show: Show)=>{
+        try{
+
+            const queryResult = await ShowDatabse.connection("BAND")
+            .select("*")
+            .where({id: show.band_id})
+
+            if(queryResult.length < 1){
+                throw new BandNotFound()
+            }
+
+            await ShowDatabse.connection
+            .insert({
+                id: show.id,
+                week_day: show.week_day,
+                start_time: show.start_time,
+                end_time: show.end_time,
+                band_id: show.band_id
+            }).into("SHOWS")
+
+        }catch(error:any){
+            throw new Error(error.message)
+        }
+    }
+
+    getShow = async(input:getShow)=>{
+        try{
+
+            const queryResult = await ShowDatabse.connection("SHOWS")
+            .select("*")
+            .where({week_day: input.week_day})
+
+            if(queryResult.length <1){
+                throw new NoShowScheduled()
+            }
+
+            return queryResult[0]
+        }catch(error:any){
+            throw new CustomError(error.statusCode, error.message)
+
+        }
+    }
+
+}
---
+++ src/error/ShowError.ts
@@ -0,0 +1,43 @@
+import { CustomError } from "./CustomError";
+
+export class BandNotFound extends CustomError{
+    constructor(){
+        super("band not found")
+    }
+}
+export class InvalidTime extends CustomError{
+    constructor(){
+        super("invalid time.")
+    }
+}
+export class InvalidStartTime extends CustomError{
+    constructor(){
+        super("start time cannot be greater than end time")
+    }
+}
+export class NotNullBandId extends CustomError{
+    constructor(){
+        super("band id is required.")
+    }
+}
+export class NotNullEndTime extends CustomError{
+    constructor(){
+        super("end end is required.")
+    }
+}
+export class NotNullStartTime extends CustomError{
+    constructor(){
+        super("start end is required.")
+    }
+}
+export class NotNullWeekDay extends CustomError{
+    constructor(){
+        super("week day is required.")
+    }
+}
+export class NoShowScheduled extends CustomError{
+    constructor(){
+        super("no shows scheduled for that day.")
+    }
+}
+
